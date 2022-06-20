a230152d762dd09fd2e33762847fb1fda23c26d6
vanhau <khaihau1002@gmail.com>
2022-06-20 22:15:02 +0700

    CSS for search and btn search

--- src/components/Layouts/components/Header/index.js
+++ src/components/Layouts/components/Header/index.js
@@ -1,14 +1,35 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCircleXmark, faMagnifyingGlass, faSpinner } from '@fortawesome/free-solid-svg-icons';
+
 import styles from './Header.module.scss';
 import classNames from 'classnames/bind';
+import images from '~/assets/images';
+
 
 const cx = classNames.bind(styles);
 
-function Header() {
-    return <header className={cx("wrapper")}>
-        <div className={cx("inner")}>
 
-        </div>
-    </header>
+function Header() {
+	return <header className={cx("wrapper")}>
+		<div className={cx("inner")}>
+			<div className={cx("logo")}>
+				<img src={images.logo} alt='TikTok' />
+			</div>
+			<div className={cx("search")}>
+				<input placeholder="Tìm kiếm tài khoản và video" />
+				<button className={cx("clear")}>
+					<FontAwesomeIcon icon={faCircleXmark} />
+				</button>
+				<FontAwesomeIcon className={cx('loading')} icon={faSpinner} />
+				<button className={cx('search-btn')}>
+					<FontAwesomeIcon icon={faMagnifyingGlass} />
+				</button>
+			</div>
+			<div className={cx("actions")}>
+				<h2>Acion</h2>
+			</div>
+		</div>
+	</header>
 }
 
 export default Header;
